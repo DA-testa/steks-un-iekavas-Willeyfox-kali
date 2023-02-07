4aab788b68e79b64b2e48c71fbe45aa86ce322b8
Oleksandr Voznenko <124575056+Willeyfox-kali@users.noreply.github.com>
2023-02-07 21:50:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ def find_mismatch(text):
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            if != opening_brackets_stack:
+            if not opening_brackets_stack:
                 return i + 1
-            if != are_matching(opening_brackets_stack.pop().char, next):
+            top = opening_brackets_stack.pop()
+            if not are_matching(top().char, next):
                 return i + 1
     if opening_brackets_stack:
          return opening_brackets_stack[0].position + 1
