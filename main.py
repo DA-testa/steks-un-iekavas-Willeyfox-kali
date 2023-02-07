e9fd5be407f05571cdd980c49ab598287a0d0584
Oleksandr Voznenko <124575056+Willeyfox-kali@users.noreply.github.com>
2023-02-07 21:56:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            opening_brackets_stack.append(Bracket(next, i)
+            opening_brackets_stack.append(Bracket(next, i))
 
         if next in ")]}":
             # Process closing bracket, write your code here
