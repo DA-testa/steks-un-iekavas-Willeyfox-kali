7ed70c9db27d49524dc611aedb8e48abff2de299
Oleksandr Voznenko <124575056+Willeyfox-kali@users.noreply.github.com>
2023-02-07 22:39:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def find_mismatch(text):
             if not are_matching(opening_brackets_stack.pop().char, next):
                 return i + 1
     if opening_brackets_stack:
-         return opening_brackets_stack[0].position + 1
+        return opening_brackets_stack[0].position + 1
     return "Success"
 
 def main():
